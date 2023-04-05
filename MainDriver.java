c8a46281a4845c879fdee7e7a2a0f6d31371fb9e
vigneshsumathi <vigneshv.java.developer@gmail.com>
2023-04-05 20:18:28 +0530

    Upload Operation Performed Successfully

---
+++ MainDriver.java
@@ -0,0 +1,10 @@
+class MainDriver{
+	public static void main(String[] args) {
+		int a =10;
+		System.out.println(a);
+		int b = 20;
+		System.out.println(b);
+		int c = a+b;
+		System.out.println(c);
+	}
+}
